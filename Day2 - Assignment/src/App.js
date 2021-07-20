5c9bf7daf63dacc3dd56bd8cd73e07b359460615
Sakshi Jadhav <56793240+sakshijadhav92@users.noreply.github.com>
2021-07-20 19:14:12 +0530

    Counter Application
    Problem Statement :
    In the counter app, the value of score should not increase more than 25.

---
+++ Day2 - Assignment/src/App.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import "./style.css"
+
+const App = ()=>{
+
+    let [score, setScore] = useState(0)
+    
+    return(
+        <div>
+             <h1> Hey this is my Counter Application </h1>
+             <h3>The value of score is {score}</h3>
+             <button onClick={()=>{score == 25?true:setScore(score+1)}}> Increment </button>
+             <button onClick={()=>(score>0)? setScore(score-1) : "" }> Decrement </button>
+             <button onClick={()=>{setScore(0)}}> Reset </button>
+        </div>
+    )
+}
+export default App
+
+
+
+
+
+
